a514658ff081de1120088641e605b8ec2543659f
achrafbokri <achrafbokri@gmail.com>
2022-08-08 06:23:08 +0200

    V3

--- inesys2.js
+++ inesys2.js
@@ -1,7 +1,7 @@
 function inesys2(Depart,Arrive) { 
      
      // (`id`, `nom`) 
-     const arrets=[[1, 'A'],[2, 'B'],[3, 'C'],[4, 'D'],[5, 'E'],[6, 'F'],[7, 'G'],[8, 'H'],[9, 'I'],[10, 'J'],[11, 'K'],[13, 'M'],[14, 'N'],[5, 'O'],[16, 'P'],[17, 'Q'],[18, 'R'],[19, 'S'],[20, 'T'],[21, 'U'],[22, 'V'],[23, 'W'],[24, 'X'],[25, 'Y'],[26, 'Z']]          
+     const arrets=[[1, 'A'],[2, 'B'],[3, 'C'],[4, 'D'],[5, 'E'],[6, 'F'],[7, 'G'],[8, 'H'],[9, 'I'],[10, 'J'],[11, 'K'],[13, 'M'],[14, 'N'],[15, 'O'],[16, 'P'],[17, 'Q'],[18, 'R'],[19, 'S'],[20, 'T'],[21, 'U'],[22, 'V'],[23, 'W'],[24, 'X'],[25, 'Y'],[26, 'Z']]          
      // (`id`, `nom`) 
      const lignes=[[1, 'Ligne 1'],[2, 'Ligne 2'],[3, 'Ligne 3'],[4, 'Ligne 4'],[4, 'Ligne 4']];
       // (`id_ligne`,`id_arret`,`num`,`sens`)
@@ -14,53 +14,94 @@ function inesys2(Depart,Arrive) {
 // return { arrets}
 let id_depart=(null);
 let id_arrive=(null);
-let depart_L=(null);
-let arrive_L=(null);
+
 
 let j=null;
 for (let i=0 ; i < arrets.length; i++){
-     if (arrets[i][1] === Depart) {
-          id_depart=arrets[i][0]
-           } else if (arrets[i][1] === Arrive) {
-          id_arrive=arrets[i][0]
-          
-     }
+     if (arrets[i][1] === Depart ) {
+          id_depart=arrets[i][0];
+           } else
+            if (arrets[i][1] === Arrive) {
+          id_arrive=arrets[i][0];
+          }
 }
-console.log(id_depart,id_arrive)
+// console.log(id_depart,id_arrive,Lignes_arrets[0][2])
+// console.log(Lignes_arrets[54][3])
 let dist=(10)
-let Dep_L=(null)
-let Dep_S=(null)
-let End_L=(null)
+let Dep_L=(null);let Dep_S=(null);let Dep_P=(null);let End_L=(null);let End_P=(null);
+let End_S=(null)
 let chemin=[]
-
 for (let i=0 ; i < Lignes_arrets.length; i++){
-     if (Lignes_arrets[i][1] === id_depart && Lignes_arrets[i][2]<dist) {
+     if (Lignes_arrets[i][1] === id_depart ) {
+          if((Lignes_arrets[i][2])<4){
           dist=Lignes_arrets[i][2]
           Dep_L=Lignes_arrets[i][0]
+          Dep_P=Lignes_arrets[i][1]
           Dep_S=Lignes_arrets[i][3]
+          }         
           }
-          else if (Lignes_arrets[i][1]===id_arrive){
-               End_L=Lignes_arrets[i][0]       
-           }
-}
-for (let i=0 ; i < Lignes_arrets.length; i++){
-     if (Lignes_arrets[i][0] === Dep_L && Lignes_arrets[i][3]===Dep_S && Lignes_arrets[i][2]<=dist) {
-          chemin.push(Lignes_arrets[i][1])
-           }     
-}
-// for (let i=0 ; i < Lignes_arrets.length; i++){
-//      if (Lignes_arrets[i][1]===id_arrive){
-//      End_L=Lignes_arrets[i][0]       
-//  }
-// }
+          else if (Lignes_arrets[i][1] === id_arrive ){
+               if((Lignes_arrets[i][2])>4){
+               End_L=Lignes_arrets[i][0]
+               End_P=Lignes_arrets[i][1]
+               End_S=Lignes_arrets[i][3]   
+          }
+         }
+       }
+       if (Dep_L===End_L){
+          chemin.push([Dep_L,Dep_S,End_P])
+      }else{
 
-console.log(`la lignes de départ est ${Dep_L} dans le sens ${Dep_S} et le chemin de départ suivant l'id arret est ${chemin.reverse()} 
-et la lignes End est ${End_L}`)
+       chemin.push([Dep_L,Dep_S],[5],[End_L,End_S,End_P])
+       
+     
+       let arret_f1=(null) 
+       let arret_f2=(null) 
+       let S_f2=(null) 
+       if (Dep_L===1){
+          if (Dep_S===0){
+               arret_f1=5
+          }else{
+               arret_f1=1
+          }
+       }
+       else if (Dep_L===2){
+            arret_f1=7
+          }else if (Dep_L===3){
+               arret_f1=5               
+          }else if (Dep_L===4){
+               arret_f1=7
+          }
+          chemin[0][2]=arret_f1
+         
 
+      if(End_L===1){
+          arret_f2=5
+      }else if(End_L===2){
+          arret_f2=7
+      }if(End_L===3){
+          arret_f2=5
+      }if(End_L===4){
+          arret_f2=7
+      }
+      
+      if(arret_f1===5){
+          S_f2=1
+      }else if(arret_f1===1){
+          S_f2=0
+      }else if (arret_f1===7){
+          if(arret_f2=5){
+               S_f2=0
+          }else { S_f2=1}
+      }
+
+      chemin[1].push(S_f2,arret_f2)
+      console.log (chemin)
+ }
 
 } 
         
 
-     let De="X";
-     let Ar="I"
+     let De="I";
+     let Ar="Q";
      inesys2(De,Ar);
